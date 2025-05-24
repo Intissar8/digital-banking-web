fd6900d8a90bc1b2ce77f10c4dc9c31b5003ce3a
Ghannane Intissar <ghannaneintissar@gmail.com>
2025-05-24 11:00:30 +0100

    first push

---
+++ src/app/customers/customers.component.ts
@@ -0,0 +1,63 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {AsyncPipe, JsonPipe, NgForOf, NgIf} from '@angular/common';
+import {CustomerService} from '../services/customer.service';
+import {catchError, Observable, throwError} from 'rxjs';
+import {Customer} from '../model/customer.model';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-customers',
+  imports: [
+    NgForOf,
+    NgIf,
+    JsonPipe,
+    AsyncPipe,
+    ReactiveFormsModule
+  ],
+  templateUrl: './customers.component.html',
+  styleUrl: './customers.component.css'
+})
+export class CustomersComponent implements OnInit{
+  customers! : Observable<Array<Customer>>;
+  errorMessage : String | undefined; //so this var will have a value or nothing or we can add to the var !
+  searchFormGroup! : FormGroup;
+  constructor(private customerService:CustomerService, private fb : FormBuilder) {
+  }
+
+  ngOnInit() { //this method executes in the debut
+    this.searchFormGroup=this.fb.group({
+      keyword : this.fb.control("")
+    });
+   this.handleSearchCustomers();//can print all of them because the search fuction prints them by default
+  }
+
+  handleSearchCustomers() {
+     let kw=this.searchFormGroup?.value.keyword//? to if it exists
+    this.customers=this.customerService.searchCustomers(kw).pipe(
+      catchError(error=>{
+        this.errorMessage=error.message;
+        return throwError(error);
+      })
+    );
+  }
+
+  handleDeleteCustomer(c: Customer) {
+    let conf = confirm("Are you sure?");
+    if(!conf) return;
+     this.customerService.deleteCustomers(c.id).subscribe({
+       next : (res)=>{
+         this.handleSearchCustomers();//to relaod the page after the deletion
+
+       },
+       error : err=>{
+         console.log(err);
+       }
+
+
+
+       }
+
+     )
+  }
+}
---
+++ src/app/services/customer.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Customer} from '../model/customer.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+  backendHost:string="http://localhost:8085";
+  constructor(private http:HttpClient) { }
+
+  //function for the search
+  public getCustomers() : Observable<Array<Customer>>/* the return type */{
+    return this.http.get<Array<Customer>>(this.backendHost+"/customers")
+  }
+
+  public searchCustomers(keyword : string) : Observable<Array<Customer>>/* the return type */{
+    return this.http.get<Array<Customer>>(this.backendHost+"/customers/search?keyword="+keyword)
+  }
+
+  public saveCustomers(customer : Customer) : Observable<Customer>/* the return type */{
+    return this.http.post<Customer>(this.backendHost+"/customers",customer)
+  }
+
+  public deleteCustomers(id : number) {
+    return this.http.delete<Customer>(this.backendHost+"/customers/"+id)
+  }
+
+
+}
